517db9e0a296d1d441374655cd569a44a79e4e95
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-05-05 12:26:55 +0100

    allowed passing of initialValue

--- AddressTextField.js
+++ AddressTextField.js
@@ -3,13 +3,10 @@ import { Form, Field } from 'react-final-form'
 
 class AddressTextField extends React.Component {
   render() {
-    const { component, label, name, required, validator} = this.props;
+    const { validator } = this.props;
     return (
       <Field
-        name={name}
-        label={label}
-        component={component}
-        required={required}
+       {...this.props}
         validate={validator}
       />
     ); 
