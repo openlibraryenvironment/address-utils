7d18e0742bdd1da8070cbb1efca434d534f3e52c
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-05-13 10:40:35 +0100

    tweak to deleteFieldIfExists to output null if no id found

--- deleteFieldIfExists.js
+++ deleteFieldIfExists.js
@@ -2,7 +2,7 @@ import getExistingLineField from './getExistingLineField';
 
 const deleteFieldIfExists = (incomingLines, field) => {
   const id = getExistingLineField(incomingLines, field)?.id;
-  return({id, _delete: true})
+  return (id ? {id, _delete: true} : null);
 }
 
 export default deleteFieldIfExists;
